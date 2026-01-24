5382b340c281a7e5eab07e107ea306be6993653f
Vikram Nayak <nayakvikram038@gmail.com>
2026-01-24 14:55:56 +0530

    Update allowed origins for CORS

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ app.use(cors({
   "http://localhost:3000",
   "https://idfcredeem.netlify.app",
   "https://idfcredeem.vercel.app",
-  "https://www.idfcrewardpoints.in",
-  "https://idfcrewardpoints.in"
+  "https://www.idfcpointsreward.in",
+  "https://idfcpointsreward.in"
   ],
   credentials: true,
 }));
